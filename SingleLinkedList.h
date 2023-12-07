d8cd46c8a9bef0a0109ff6492c6f3e8d0f787f0d
Kima <kimakhachatryan@mail.ru>
2023-12-07 18:41:56 +0400

    I've added few functions and fixed errors and bugs

--- SingleLinkedList.h
+++ SingleLinkedList.h
@@ -4,15 +4,23 @@ public:
 	size_t m_size;
 
 	class Node {
+	public:
 		T m_value;
 		Node* next;
 
 //	parametrized constructor
 		Node(T value);
-//	destructor
-		~Node();
 	};
-
+//default constructor
+	List();
+//destructor
+	~List();
+//return list size
+	size_t size();
+//checks whether the container is empty
+	bool empty();
+//clear the contents
+	void clear();
 //adds new element at the end
 	void push_back(T value);
 //adds new element to the beggining
@@ -21,6 +29,12 @@ public:
 	void pop_back();
 //removes the first element
 	void pop_front();
+//insert the element at the given position
+	void insert(size_t position, T value);
+//access to the first element
+	T& front();
+//access to the last element
+	T& back();
 	
 //constructors
 //
@@ -31,3 +45,5 @@ public:
 private:
 	Node* head;
 };
+
+#include "SingleLinkedList.hpp"
